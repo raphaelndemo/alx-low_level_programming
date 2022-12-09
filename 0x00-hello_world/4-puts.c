df5e7184a4c3f31094bcf67bac85d8e901209b9d
raphaelndemo <ndemoraphael4@gmail.com>
2022-12-09 12:43:45 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main -Entry
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 int main (void)
 {
-	puts("\"Programming is like building a multi lingual puzzle);
-	return(0);
+	puts("\"Programming is like building a multi lingual puzzle");
+	return (0);
 }
